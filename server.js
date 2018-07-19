ffce06583c0012760847eb96b85a58bf76988f9e
Javier Exposito G <javiexpo2003@gmail.com>
2018-07-19 08:56:51 +0200

    Initial commit - Simple NodeJS Server setup

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const PORT = 5000;
+const app = express();
+
+// app.use(bodyParser);
+
+app.get('/publish', function (req, res) {
+    console.log('GET on /publish');
+    res.status(200)
+        .send('Node JS Server is running and waiting for request'); //Ping for SM59 Test
+});
+
+app.get('*', function (req, res) {
+    res.sendStatus(404);
+});
+
+app.post('/publish', function (req, res) {
+    const jsonData = JSON.stringify(req.body);
+    console.log(`SAP message: ${jsonData}`);
+    res.send(200);
+});
+
+app.listen(PORT, function () {
+    console.log('Server is running and listening on port 5000');
+});
