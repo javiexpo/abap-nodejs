56cca06553e624e9bc7d9a1b7bf4484508a19f36
Javier Exposito G <javiexpo2003@gmail.com>
2018-07-23 16:15:07 +0200

    Adding .env config file

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 // app.use(bodyParser);
+console.log(process.env.PORT);
 
 app.get('/publish', function (req, res) {
     console.log('GET on /publish');
